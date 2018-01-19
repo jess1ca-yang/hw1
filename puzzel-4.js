27637c1d3a25eb7381348cc533b27d50ee0b6a32
Jessica Yang <jessica.yang@cca.edu>
2018-01-18 23:51:00 -0800

    Create puzzel-4.js

---
+++ puzzel-4.js
@@ -0,0 +1,22 @@
+function repeat(direction, n) {
+  var count = 0
+  while (count < n)
+  direction
+  count = count + 1
+}
+
+repeat(down, 4)
+right
+getColor
+setColor = getColor
+down
+setColor
+repeat(right, 2)
+up
+right
+getColor
+setColor = getColor
+down
+setColor
+right
+right
