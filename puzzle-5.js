86960e1cb77fd2cdba18918272e864fb2f985013
Jessica Yang <jessica.yang@cca.edu>
2018-01-18 23:52:55 -0800

    Create puzzle-5.js

---
+++ puzzle-5.js
@@ -0,0 +1,29 @@
+function repeat(direction, n) {
+  var count = 0
+  while (count < n)
+  direction
+  count = count + 1
+}
+
+repeat(right, 2)
+if (getColor == "blue") {
+  down
+}
+else {
+  up
+}
+repeat(right, 2)
+if (getColor == "blue") {
+  down
+}
+else {
+  up
+}
+repeat(right, 2)
+if (getColor == "blue") {
+  down
+}
+else {
+  up
+}
+repeat(right, 3)
