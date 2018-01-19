8669813f41144daedbed543da2b7ac8e1a98a034
Jessica Yang <jessica.yang@cca.edu>
2018-01-18 23:57:33 -0800

    Create puzzle-9.js

---
+++ puzzle-9.js
@@ -0,0 +1,33 @@
+function repeat(direction, n) {
+  var count = 0
+  while (count < n)
+  direction
+  count = count + 1
+}
+
+var count = 0
+while (count < 9) {
+  setColor = "blue"
+  right
+  count = count + 1
+}
+down
+var count = 0
+while (count < 3) {
+  setColor = "blue"
+  down
+  count = count + 1
+}
+left
+var count = 0
+while (count < 8) {
+  setColor = "blue"
+  left
+  count = count + 1
+}
+up
+var count = 0
+while (getColor != "blue") {
+  setColor = "blue"
+  up
+}
