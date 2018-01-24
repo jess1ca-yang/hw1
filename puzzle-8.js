858e5cb8027194437274f315105327e0e2102bbd
Jessica Yang <jessica.yang@cca.edu>
2018-01-23 20:52:21 -0800

    Update puzzle-8.js

--- puzzle-8.js
+++ puzzle-8.js
@@ -1,10 +1,10 @@
 function repeat(direction, n) {
   var count = 0
-  while (count < n)
+  while (count < n) {
   direction
   count = count + 1
+  }
 }
-
 repeat(down, 8)
 repeat(right, 2)
 repeat(up, 8)
