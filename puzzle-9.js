45d06f31bb648cf0aec71833d4773a78e57a3728
Jessica Yang <jessica.yang@cca.edu>
2018-01-24 10:06:48 -0800

    Update puzzle-9.js

--- puzzle-9.js
+++ puzzle-9.js
@@ -1,34 +1,34 @@
+
+
 function repeat(direction, n) {
   var count = 0
   while (count < n) {
-  direction
+  direction();
   count = count + 1
   }
+  count = 0
 }
 
-var count = 0
-while (count < 9) {
-  setColor = "blue"
-  right
+function draw(direction, n) {
+  var count = 0
+	while (count < n) {
+  setColor("blue");
+  direction();
   count = count + 1
+	}
 }
-down
-var count = 0
-while (count < 3) {
-  setColor = "blue"
-  down
-  count = count + 1
+
+
+function rect(x, y, width, height) {
+  var count = 0
+  repeat(right, x);
+  repeat(down, y);
+	draw(right, width);
+  draw(down, height);
+	draw(left, width);
+  draw(up, height);
+  
 }
-left
-var count = 0
-while (count < 8) {
-  setColor = "blue"
-  left
-  count = count + 1
-}
-up
-var count = 0
-while (getColor != "blue") {
-  setColor = "blue"
-  up
-}
+
+
+rect(2, 3, 5, 15);
